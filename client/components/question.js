1c9a6ea0edaf24606fe8edc44e854cee04548b22
Eli Mauskopf <elimauskopf7@gmail.com>
2018-08-08 15:50:13 -0400

    started question component

---
+++ client/components/question.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {logout} from '../store'
+
+
+export default class Question extends  React.Component {
+
+    constructor(){
+        super()
+    }
+
+    render(){
+        return (
+            <p> hi </p>
+        )
+    }
+}
+
