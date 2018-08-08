afb9c7b25137c56c567e6445dd736a9f398e40b0
Brandon-Lao <brandon.lao11@gmail.com>
2018-08-08 16:59:38 -0400

    LInks from user home to the question work. Currently stateful, will need to be converted to storeful possibly.

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import axios from 'axios'
 
 /**
  * COMPONENT
  */
-export const UserHome = props => {
-  const {email} = props
+export class UserHome extends Component {
+  constructor() {
+    super()
+    this.state = {
+      topics: []
+    }
+  }
 
-  return (
-    <div>
-      <h3>Welcome, {email}</h3>
-    </div>
-  )
+  async componentDidMount() {
+    let topics = await axios.get('/api/questions')
+    this.setState({
+      topics: topics.data
+    })
+  }
+
+  render() {
+    while (this.state.topics === []) {
+      return (
+        <div>
+          <h3>Loading...</h3>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <h1>Topics Available for Study</h1>
+        {this.state.topics.map((maintopic, index) => {
+          return <Link to={'/'+(index+1)} key={maintopic.name} params={{index: index}}><h3>{maintopic.name}</h3></Link>
+        })}
+      </div>
+    )
+  }
 }
 
 /**
