bcc06c9484720f636028ec178a94176819ffb0ab
Lawrence Lin <lawrence.y.lin@gmail.com>
2018-08-09 16:19:05 -0400

    questions

--- client/components/dumbQuestion.js
+++ client/components/dumbQuestion.js
@@ -5,69 +5,60 @@ import {dispatch} from 'redux'
 import {fetchQuestions} from '../store/questions'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
-import { List, Button, Header, Container, Message } from 'semantic-ui-react'
+import {List, Button, Header, Container, Message} from 'semantic-ui-react'
 
+class DumbQuestion extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  // console.log('PROPS',props)
+  // let clickHandlerCounter =0;
+  // console.log(props.clickHandlers)
 
-class  DumbQuestion extends React.Component {
-
-    constructor(props){
-        super(props);
-        this.state = {}
+  render() {
+    for (let i = 0; i < this.props.answers; i++) {
+      this.setState({[answer.id]: false})
     }
-    // console.log('PROPS',props)
-    // let clickHandlerCounter =0;
-    // console.log(props.clickHandlers)
-    
-    render(){
-        for (let i =0; i<this.props.answers;i++){
-            this.setState({[answer.id]: false})
-        }
-        console.log('DQ PROPS',this.props)
-       
-        return (
-            <div>
-                
-                
-            <Container>
-                <Header size='large'>{ this.props.question }</Header>
-                <List animated relaxed verticalAlign='middle'>
-                {this.props.answers.map(answer => {
-                    let show = false;
-                       return (
-                           
-                            <div>
-                               <List.Item key={answer.id}  >
-                                   <List.Content>
-                                       <Button onClick={() => {
-                                           this.setState({[answer.id]:true})
-                                       }} size='large' basic>{answer.answerText}</Button>
-                                   </List.Content>
-                               </List.Item>
-                               {this.state[answer.id] && <Message visible> {answer.explanationText}</Message>}
-                               <div>
-                                   <Link to={this.props.linkToNext}> GO NEXT </Link>
-                                </div>
-                               
-                            </div>
-                          
-                       )
-                       clickHandlerCounter++;
-                       }
-                         
-                        
-                )}
-                 </List> 
-                     
-    
-            </Container>
-    
-        </div>
-           
-        )
-    }
-    
+    console.log('DQ PROPS', this.props)
+
+    return (
+      <div>
+        <Container>
+          <Header size="large">{this.props.question}</Header>
+          <List animated relaxed verticalAlign="middle">
+            {this.props.answers.map(answer => {
+              let show = false
+              return (
+                <div>
+                  <List.Item key={answer.id}>
+                    <List.Content>
+                      <Button
+                        onClick={() => {
+                          this.setState({[answer.id]: true})
+                        }}
+                        size="large"
+                        basic
+                      >
+                        {answer.answerText}
+                      </Button>
+                    </List.Content>
+                  </List.Item>
+                  {this.state[answer.id] && (
+                    <Message visible> {answer.explanationText}</Message>
+                  )}
+                  <div>
+                    <Link to={this.props.linkToNext}> GO NEXT </Link>
+                  </div>
+                </div>
+              )
+              clickHandlerCounter++
+            })}
+          </List>
+        </Container>
+      </div>
+    )
+  }
 }
-
-
 
 export default DumbQuestion
--- client/components/smartQuestion.js
+++ client/components/smartQuestion.js
@@ -1,98 +1,195 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {dispatch} from 'redux'
 import {fetchQuestions} from '../store/questions'
 import {Link} from 'react-router-dom'
-import {logout} from '../store'
-import DumbQuestion from './dumbQuestion'
-import { List, Button, Header, Container, Segment, Dimmer, Loader } from 'semantic-ui-react'
 
+import {List, Button, Header, Container, Message} from 'semantic-ui-react'
 
-class Question extends  React.Component {
-
-    constructor(props){
-        super(props)
-        this.state = {
-            loaded: false,
-            question: '',
-            answers: '',
-            currentLink: this.props.history.location.pathname,
-            linkToNext: ''
-        }
+class Question extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loaded: false,
+      question: '',
+      answers: '',
+      nextMode: 'repeat',
+      linkToNext: false,
+      showNext: false
     }
+    this.changeMode = this.changeMode.bind(this)
+  }
 
-    async componentWillMount(){
-         await this.props.getAllQuestions()
-        
-            
-        //Find next link
-        let linkToNext ='';
-        // console.log('STATE', this.state)
-        // console.log('PATH', this.props.history.location.pathname)
-        if(this.state.currentLink.includes('repeat')){
-            this.setState({linkToNext:'/example'})
-            this.setState({question: this.props.questions[0].SubTopics[0].Questions[0].QuestionList.RQuestion})
-            this.setState({answers: this.props.questions[0].SubTopics[0].Questions[0].QuestionList.RQuestions})
-            this.setState({loaded: true})
-        } else if(this.state.currentLink.includes('example')){
-            this.setState({linkToNext: '/approach'})
-            this.setState({question: this.props.questions[0].SubTopics[0].Questions[0].QuestionList.EQuestion})
-            this.setState({answers: this.props.questions[0].SubTopics[0].Questions[0].QuestionList.EQuestions})
-            this.setState({loaded: true})
-        } else {
-            this.setState({linkToNext: '/editor'})
-            this.setState({question: this.props.questions[0].SubTopics[0].Questions[0].QuestionList.AQuestion})
-            this.setState({answers: this.props.questions[0].SubTopics[0].Questions[0].QuestionList.AQuestions})
-            this.setState({loaded: true})
-        }
-         
-         
-    }
+  async componentDidMount() {
+    await this.props.getAllQuestions()
+    let root = this.props.questions[0].SubTopics[0].Questions[0].QuestionList
 
-    render(){
-        
+    this.setState({
+      question: root.RQuestion,
+      answers: root.RQuestions,
+      loaded: true
+    })
+  }
 
-
-
-        let clickHandlerArr = [];
-        if(this.state.loaded){
-            
-            this.state.answers.forEach(element => {
-                clickHandlerArr.push(function(element){
-                    console.log(element.correct)
-                });
+  componentDidUpdate(prevProps, prevState) {
+    // console.log('prevProps', prevProps.match.url)
+    // console.log('prevState', prevState.nextMode)
+    // console.log('nextProps', this.props)
+    console.log('nextState', this.state)
+    if (prevState.nextMode !== this.state.nextMode) {
+      let root = this.props.questions[0].SubTopics[0].Questions[0].QuestionList
+      if (this.state.nextMode === 'approach') {
+        this.setState({
+          question: root.AQuestion,
+          answers: root.AQuestions,
+          loaded: true
         })
+      } else if (this.state.nextMode === 'example') {
+        this.setState({
+          question: root.EQuestion,
+          answers: root.EQuestions,
+          loaded: true
+        })
+      }
+    } else if (prevProps.location.pathname !== this.props.location.pathname) {
+      console.log('props did run')
+      let root = this.props.questions[0].SubTopics[0].Questions[0].QuestionList
+      const path = this.props.location.pathname
+      if (path.endsWith('repeat')) {
+        this.setState({
+          question: root.RQuestion,
+          answers: root.RQuestions,
+          nextMode: 'example',
+          loaded: true
+        })
+      } else if (path.endsWith('example')) {
+        this.setState({
+          question: root.EQuestion,
+          answers: root.EQuestions,
+          nextMode: 'approach',
+          loaded: true
+        })
+      } else if (path.endsWith('approach')) {
+        this.setState({
+          question: root.AQuestion,
+          answers: root.AQuestions,
+          nextMode: 'editor',
+          loaded: true
+        })
+      }
     }
-       
-       
-        
-        return (
-            <div>
-            {this.state.loaded &&
+  }
 
-            <DumbQuestion question={this.state.question} answers ={this.state.answers} clickHandlers ={clickHandlerArr} linkToNext={this.props.history.location.pathname + this.state.linkToNext}/>
-            }
-        </div>
-            
-        )
-           
-    
+  changeMode() {
+    if (this.state.nextMode === 'repeat') {
+      this.setState({nextMode: 'example'})
+    } else if (this.state.nextMode === 'example') {
+      this.setState({nextMode: 'approach'})
+    } else if (this.state.nextMode === 'approach') {
+      this.setState({nextMode: 'editor'})
+    }
+    console.log('i changed mode')
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.loaded && (
+          <div>
+            <Container>
+              <Header size="large">{this.state.question}</Header>
+              <List animated relaxed verticalAlign="middle">
+                {this.state.answers.map(answer => {
+                  return (
+                    <div key={answer.id}>
+                      <List.Item>
+                        <List.Content>
+                          <Button
+                            onClick={() => {
+                              this.setState({[answer.id]: true})
+                            }}
+                            size="large"
+                            basic
+                          >
+                            {answer.answerText}
+                          </Button>
+                        </List.Content>
+                      </List.Item>
+                      {this.state[answer.id] && (
+                        <React.Fragment>
+                          <Message visible> {answer.explanationText}</Message>
+                          {answer.correct &&
+                            this.props.history.location.pathname.endsWith(
+                              'repeat'
+                            ) && (
+                              <Link
+                                to={
+                                  this.props.history.location.pathname +
+                                  '/example'
+                                }
+                              >
+                                <Button
+                                  onClick={() => {
+                                    this.setState({[answer.id]: false})
+                                    this.changeMode()
+                                  }}
+                                >
+                                  GO NEXT
+                                </Button>
+                              </Link>
+                            )}
+                          {answer.correct &&
+                            this.props.history.location.pathname.endsWith(
+                              'example'
+                            ) && (
+                              <Link
+                                to={
+                                  this.props.history.location.pathname +
+                                  '/approach'
+                                }
+                              >
+                                <Button
+                                  onClick={() => {
+                                    this.setState({[answer.id]: false})
+                                    this.changeMode()
+                                  }}
+                                >
+                                  GO NEXT
+                                </Button>
+                              </Link>
+                            )}
+                        </React.Fragment>
+                      )}
+                    </div>
+                  )
+                })}
+              </List>
+            </Container>
+          </div>
+        )}
+      </div>
+    )
+  }
 }
-}   
 
-
-const mapStateToProps = (state) => {
-    return {
-        questions: state.questions,
-       
-    }
+const mapStateToProps = state => {
+  return {
+    questions: state.questions
+  }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getAllQuestions: () => dispatch(fetchQuestions())
-    }
+const mapDispatchToProps = dispatch => {
+  return {
+    getAllQuestions: () => dispatch(fetchQuestions())
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Question)
+
+// <DumbQuestion
+//   question={this.state.question}
+//   answers={this.state.answers}
+//   clickHandlers={clickHandlerArr}
+//   linkToNext={
+//     this.props.history.location.pathname + this.state.linkToNext
+//   }
+// />
