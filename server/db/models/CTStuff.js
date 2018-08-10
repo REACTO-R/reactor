167c9be0ea0a01c7836ad93af62f2d70ff057991
Brandon-Lao <brandon.lao11@gmail.com>
2018-08-10 15:17:17 -0400

    CTStuff changed to have an input and output property. Inputs are stored as stringified arrays.

--- server/db/models/CTStuff.js
+++ server/db/models/CTStuff.js
@@ -2,9 +2,12 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const CTStuff = db.define('CTStuff', {
-	Code: {
+	Input: {
 		type: Sequelize.TEXT
 	},
+	Output: {
+		type: Sequelize.TEXT
+	}
 })
 
 module.exports = CTStuff
