c370cdecf0bc4e80eb6b8c08949a684264c3ab90
Lawrence Lin <lawrence.y.lin@gmail.com>
2018-08-20 16:17:09 -0400

    tests

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -7,10 +7,11 @@ const request = require('supertest')(app)
 const User = db.model('user')
 
 describe('User routes', () => {
-  console.log("Starting.")
+  console.log('Starting.')
   beforeEach(() => {
     return db.sync({force: true})
-  })})
+  })
+})
 
 const userCredentials = {
   email: 'cody@email.com',
@@ -19,7 +20,7 @@ const userCredentials = {
 
 let cookieJar
 
-describe('useless api endpoint', function() {
+describe('authenticated access only', function() {
   before(function(done) {
     request
       .post('/auth/login')
@@ -31,82 +32,12 @@ describe('useless api endpoint', function() {
       })
   })
 
-  it('posts an xobject', async () => {
+  it('posts an object', async () => {
     const res = await request
       .get('/api/users')
       .set('Cookie', cookieJar)
       .expect(200)
-
-    console.log('res', res.body)
-    expect(res.body.length).to.equal(2)
+    // console.log('res', res.body)
+    expect(res.body.length).to.equal(1)
   })
 })
-
-// var authenticatedUser = request.agent(app)
-// before(function(done) {
-//   authenticatedUser
-//     .post('/login')
-//     .send(userCredentials)
-//     .end(function(err, response) {
-//       // console.log('response', response)
-//       expect(response.statusCode).to.equal(200)
-//       done()
-//     })
-// })
-
-// describe('GET /profile', function(done) {
-//addresses 1st bullet point: if the user is logged in we should get a 200 status code
-//   it('should return a 200 response if the user is logged in', function(done) {
-//     authenticatedUser.get('/api/users').expect(200, done)
-//   })
-// })
-
-// describe('User routes', () => {
-//   // beforeEach(() => {
-//   //   return db.sync({force: true})
-//   // })
-
-//   describe('/api/users/ ', () => {
-//     const codysEmail = 'cody@email.com'
-
-//     // beforeEach(() => {
-//     //   return User.create({
-//     //     email: codysEmail
-//     //   })
-//     // })
-
-//     it('GET /api/users', async () => {
-//       const res = await request(app)
-//         .get('/api/users')
-//         .expect(200)
-//       console.log('res', res)
-//       expect(res.body).to.be.an('array')
-//       expect(res.body[0].email).to.be.equal(codysEmail)
-//     })
-//   })
-// })
-
-// describe('User routes', () => {
-//   beforeEach(() => {
-//     return db.sync({force: true})
-//   })
-
-//   describe('/api/users/', () => {
-//     const codysEmail = 'cody@puppybook.com'
-
-//     beforeEach(() => {
-//       return User.create({
-//         email: codysEmail
-//       })
-//     })
-
-//     it('GET /api/users', async () => {
-//       const res = await request(app)
-//         .get('/api/users')
-//         .expect(200)
-
-//       expect(res.body).to.be.an('array')
-//       expect(res.body[0].email).to.be.equal(codysEmail)
-//     })
-//   }) // end describe('/api/users')
-// }) // end describe('User routes')
